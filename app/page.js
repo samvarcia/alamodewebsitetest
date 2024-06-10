0209014249e1e143618c5ff7357cc0852d994d7d
Samuel Garcia <samvarcia@gmail.com>
2024-06-10 16:41:51 +0100

    First test

--- app/page.js
+++ app/page.js
@@ -1,94 +1,59 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { motion, useAnimation } from 'framer-motion';
+import Link from "next/link";
 
 export default function Home() {
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+        
+        <div className={styles.heroContent}>
+          <Image 
+            src="/logoalamode.svg"
+            width={250}
+            height={100}
+          />
+          <div className={styles.heroText}>
+            <h3>NEW YORK, LONDON,<br/> MILAN, PARIS</h3>
+            <p>...PRACTICING MY MODEL WALK</p>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className={styles.slides}>
+      <motion.section className={styles.hero}
+        animate={{
+          background:["radial-gradient(115.53% 100% at 50% 0%, rgba(0, 0, 0, 0.14) 25%, rgba(188, 1, 35, 1) 100%), #000",
+          "radial-gradient(115.53% 100% at 50% 0%, rgba(0, 0, 0, 0.57) 30.62%, #BC0123 52.24%), #000", 
+          "radial-gradient(115.53% 100% at 50% 0%, rgba(0, 0, 0, 0.14) 25%, rgba(188, 1, 35, 1) 100%), #000",]
+        }}
+        transition={{ ease: "easeInOut", duration: 5 }}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.aboutLink}>
+          ABOUT
+        </div>
+      </motion.section>
+        <section className={styles.slideOne}>
+          <div className={styles.slideContent}>
+          </div>
+        </section>
+        <section className={styles.slideTwo}>
+          <div className={styles.slideContent}>
+          </div>
+        </section>
+        <section className={styles.slideThree}>
+          <div className={styles.slideContent}>
+          </div>
+        </section>
+        <section className={styles.slideFour}>
+          <div className={styles.slideContent}>
+          </div>
+        </section>
+        <section className={styles.slideFive}>
+          <div className={styles.slideContent}>
+          </div>
+        </section>
       </div>
     </main>
   );
