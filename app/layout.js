3c6861253147b9b66c1b8f4d6ebf0bdb4ceb4722
Samuel Garcia <samvarcia@gmail.com>
2024-07-08 12:06:00 +0100

    ICONNNN

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
+	      <link rel="icon" href="/icon.ico" type="imag/x-icon" sizes="16x16"/>
+       </head>
       <body className={futura.className}>{children}</body>
     </html>
   );
