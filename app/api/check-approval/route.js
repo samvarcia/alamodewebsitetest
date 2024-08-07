58a193cb46d463a6bd36868b42f81c5ba00d114b
Samuel Garcia <samvarcia@gmail.com>
2024-08-07 09:56:31 +0100

    fuck it we ball3

--- app/api/check-approval/route.js
+++ app/api/check-approval/route.js
@@ -1,4 +1,4 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 // import { google } from 'googleapis';
 // import nodemailer from 'nodemailer';
 // import { v4 as uuidv4 } from 'uuid';
