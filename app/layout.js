f1837dfa7f513796dd1429a1f8c227dd56c6257e
Samuel Garcia <samvarcia@gmail.com>
2024-08-29 10:56:43 -0500

    fonts and stuff

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,11 @@
 import localFont from "next/font/local";
-// import { Inter } from "next/font/google";
 import "./globals.css";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "à la mode",
   description: "Location à la mode is a model exclusive event series created to foster a sense of community within the often isolating modeling industry",
 };
+
 const futura = localFont({
   src: [
     {
@@ -22,13 +20,15 @@ const futura = localFont({
     },
   ],
   variable: '--font-futura',
+  display: 'swap', // Add this line
 });
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <head>
-	      <link rel="icon" href="/icon.ico" type="imag/x-icon" sizes="16x16"/>
-       </head>
+        <link rel="icon" href="/icon.ico" type="image/x-icon" sizes="16x16"/>
+      </head>
       <body className={futura.className}>{children}</body>
     </html>
   );
