fa31f1f3169aae98587b19b3499c6553bcd91897
Samuel Garcia <samvarcia@gmail.com>
2024-08-30 22:22:03 -0500

    lets go

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    experimental: {
-      serverComponentsExternalPackages: ['@react-pdf/renderer'],
-    },
-    webpack: (config, { isServer }) => {
-      if (isServer) {
-        config.externals.push('@react-pdf/renderer');
-      }
+    
+    webpack: (config) => {
+      config.resolve.alias.canvas = false;
       return config;
     },
   }
