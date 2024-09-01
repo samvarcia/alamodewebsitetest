40e7e119c0110be3272e00fdcc5eb25a7bfd94a5
Samuel Garcia <samvarcia@gmail.com>
2024-08-31 19:32:22 -0500

    mayyybeee

--- app/api/check-approval/route.js
+++ app/api/check-approval/route.js
@@ -70,8 +70,8 @@ export async function GET(request) {
       },
       backgroundImage: {
         position: 'absolute',
-        width: '100vw',
-        height: '100vh',
+        width: '100%',
+        height: '100%',
         left: 0,
         top: 0,
       },
