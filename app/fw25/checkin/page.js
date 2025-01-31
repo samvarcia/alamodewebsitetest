956f4b972da77658d2b3e3f55c1cfaf3775baffd
Samuel Garcia <samvarcia@gmail.com>
2025-01-31 10:56:27 -0500

    initial checkin map

---
+++ app/fw25/checkin/page.js
@@ -0,0 +1,30 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import styles from './page.module.css';
+import { motion, useAnimation } from 'framer-motion';
+import Image from 'next/image';
+import WorldMap from '@/app/components/WorldMap';
+
+const checkin = () => {
+ 
+  return (
+    <div className={styles.container}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ duration: 0.3 }}
+        style={{ position: 'fixed', bottom: 20, right: 20, zIndex: 2 }}
+      >
+        <Image 
+          src="https://5b4ey7iavy.ufs.sh/f/sPxirgcVYJN5ziOWkkGWGeO9CyJiqhFg5S3kH6Q8afZc0DB1"
+          width={150}
+          height={100}
+          priority
+          className={styles.fixedLogo}
+        />
+      </motion.div>
+      <WorldMap/>
+    </div>
+  );
+};
+
+export default checkin;
